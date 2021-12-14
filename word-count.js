035373ee866af6ae6d40cf991e724011edbe091f
Mike Lynch <mjlynch2@gmail.com>
2021-12-14 12:04:29 -0600

    initial attempt; regex match, iterate, count

--- word-count.js
+++ word-count.js
@@ -1,8 +1,15 @@
-//
-// This is only a SKELETON file for the 'Word Count' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-export const countWords = () => {
-  throw new Error('Remove this statement and implement this function');
+export const countWords = (phrase) => {
+  const regex = /\S([a-zA-Z']|[0-9])+/g;
+  let wordList = phrase.match(regex);
+  let resultList = [];
+  let resultCount = {};
+  wordList.forEach(e => {
+    if(resultList.includes(e)){
+      resultCount[e]++;
+    } else {
+      resultList.push(e);
+      resultCount[String(e)] = 1;
+    }
+  })
+  return resultCount;
 };
